56a7ebc3118dec1c62b36f145d0dc0a081942ece
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-09-25 09:03:42 +0700

    Setup: initialize

---
+++ src/main/java/Xanadu/XanaduApplication.java
@@ -0,0 +1,13 @@
+package Xanadu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XanaduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XanaduApplication.class, args);
+	}
+
+}
