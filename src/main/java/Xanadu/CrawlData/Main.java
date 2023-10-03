d156141aba56ed9057ca4ffaf7af441a6aaef8b1
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-10-03 11:23:05 +0700

    Fix: jpaAuditingHandler beab duplicate

--- src/main/java/Xanadu/CrawlData/Main.java
+++ src/main/java/Xanadu/CrawlData/Main.java
@@ -4,6 +4,7 @@ import Xanadu.Entities.*;
 import Xanadu.Entities.Collection;
 import Xanadu.Services.*;
 
+import Xanadu.XanaduApplication;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -21,7 +23,13 @@ import java.util.*;
 
 @Slf4j
 @SpringBootApplication
-@ComponentScan(basePackages = {"Xanadu", "Xanadu/CrawlData"})
+@ComponentScan(
+        basePackages = {"Xanadu", "Xanadu/CrawlData"},
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.CUSTOM,
+                classes = XanaduApplication.class
+        )
+)
 @EnableJpaAuditing
 @EnableTransactionManagement
 public class Main implements ApplicationRunner {
@@ -62,6 +70,7 @@ public class Main implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws URISyntaxException, InterruptedException {
         crawling();
+        log.info("crawled.");
     }
 
     private void crawling() throws InterruptedException, URISyntaxException {
--- src/main/java/Xanadu/XanaduApplication.java
+++ src/main/java/Xanadu/XanaduApplication.java
@@ -2,14 +2,23 @@ package Xanadu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@EnableJpaAuditing
+@ComponentScan(
+        basePackages = "Xanadu",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = "Xanadu.CrawlData..*"
+        )
+)
 public class XanaduApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XanaduApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XanaduApplication.class, args);
+    }
 
 }
