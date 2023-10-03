1cdbd5aee990494e9b724a7e3542fbef0bdaa238
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-10-04 00:16:48 +0700

    Feature: created admin layout page

---
+++ src/main/java/Xanadu/Controllers/Admin/HomeController.java
@@ -0,0 +1,25 @@
+package Xanadu.Controllers.Admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/admin")
+@Slf4j
+public class HomeController {
+
+    @GetMapping(value = {"home.html"})
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Home");
+        return "admin/index";
+    }
+
+    @RequestMapping(value = {"/home", "/home/**", "", "'index", "index.html"})
+    public String redirectToHomePage() {
+        return "redirect:/admin/home.html";
+    }
+}
