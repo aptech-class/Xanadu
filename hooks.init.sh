7d28a0d6ca0b36593d17e7548825eb4325215773
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-01-06 00:14:35 +0700

    Refactor: edited git hooks and update database init

--- hooks.init.sh
+++ hooks.init.sh
@@ -2,11 +2,11 @@
 
 chmod +x ./hooks/pre-commit.sh
 
-if ! [[ -f "./.git/hooks/pre-commit" ]]; then
+if  [ ! -f "./.git/hooks/pre-commit" ]; then
   ln ./hooks/pre-commit.sh ./.git/hooks/pre-commit
 fi
 chmod +x ./hooks/commit-msg.sh
-if ! [[ -f "./.git/hooks/commit-msg" ]]; then
+if [ ! -f "./.git/hooks/commit-msg" ]; then
   ln ./hooks/commit-msg.sh ./.git/hooks/commit-msg
 
 fi
